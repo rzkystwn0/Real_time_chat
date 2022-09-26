196ab5fd7f39320a5aaed400b5f01dc492b93850
rzkystwn0 <103617822+rzkystwn0@users.noreply.github.com>
2022-09-26 19:46:35 +0700

    Delete dfg.js

--- dfg.js
+++
@@ -1,5 +0,0 @@
-['my-2', 'float-right', 'border', 'border-blue-600', 'sm:max-w-md', 'max-w-xs', 'mx-2', 'rounded-t-xl', 'rounded-l-xl', 'px-4', 'text-right', 'dark:text-white']
-
-['my-2 border', 'border-green-500', 'sm:max-w-md', 'max-w-xs', 'mx-2', 'rounded-t-xl', 'rounded-r-xl, px-4', 'dark:text-white']
-
-'my-2', border', 'border-green-500', 'sm:max-w-md', 'max-w-xs', 'mx-2', 'rounded-t-xl', 'rounded-r-xl, px-4', 'dark:text-white'
